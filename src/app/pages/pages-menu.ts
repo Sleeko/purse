88719829ecd479db4e0709083aa9d0fdc64cba47
arjay <arjay630@gmail.com>
2019-07-15 09:41:17 +0800

    dashaboard and update to ngxadmin

---
+++ src/app/pages/pages-menu.ts
@@ -0,0 +1,56 @@
+import { NbMenuItem } from '@nebular/theme';
+
+export const MENU_ITEMS: NbMenuItem[] = [
+  {
+    title: 'Home',
+    icon: 'home-outline',
+    link: '/pages/dashboard',
+    home: true,
+  },
+  {
+    title: 'Virtual Chamber',
+    icon: 'home-outline',
+    link: '/pages/iot-dashboard',
+  },
+
+  {
+    title: 'Layaway Purse',
+    icon: 'home-outline',
+    link: '/pages/iot-dashboard',
+  },
+  {
+    title: 'Re-Entry Purse',
+    icon: 'log-in-outline',
+    link: '/pages/iot-dashboard',
+  },
+  {
+    title: 'Level-up Purse',
+    icon: 'corner-right-up-outline',
+    link: '/pages/iot-dashboard',
+  },
+  {
+    title: 'Cash Purse',
+    icon: 'home-outline',
+    link: '/pages/iot-dashboard',
+  },
+  {
+    title: 'Maintenance Purse',
+    icon: 'settings-2-outline',
+    link: '/pages/iot-dashboard',
+  },
+  {
+    title: 'Account Settings',
+    icon: 'person-outline',
+    link: '/pages/iot-dashboard',
+  },
+  {
+    title: 'FAQ',
+    icon: 'question-mark-circle-outline',
+    link: '/pages/iot-dashboard',
+  },
+  {
+    title: 'Terms and Condition',
+    icon: 'file-text-outline',
+    link: '/pages/iot-dashboard',
+  }
+];
