df2c28edad4eb11ee23ab58c8d95b4822914b764
arjay <arjay630@gmail.com>
2019-10-12 15:36:47 +0800

    Account Settings- initial commit complete registration

--- src/app/views/account-settings/account-settings.module.ts
+++ src/app/views/account-settings/account-settings.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { ChartsModule } from 'ng2-charts';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AccountSettingsComponent } from './account-settings.component';
 import { AccountSettingsRoutingModule } from './account-settings-routing.module';
 
@@ -13,6 +13,8 @@ import { AccountSettingsRoutingModule } from './account-settings-routing.module'
     AccountSettingsRoutingModule,
     ChartsModule,
     BsDropdownModule,
+    CommonModule,
+    ReactiveFormsModule,
     ButtonsModule.forRoot()
   ],
   declarations: [ AccountSettingsComponent ]
